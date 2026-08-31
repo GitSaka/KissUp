1dff97126e1b451138423a44567dd3c12b4eb6e7
GitSaka <sakaayedidjou@gmail.com>
2026-08-31 20:25:39 +0200

    feat(profile): add avatar update, backend profile route, and fix gallery image mapping

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,17 +2,18 @@ import { Router } from 'express';
 import { verifyJwt } from '../middlewares/authMiddleware.js';
 import { addUserPhoto, deleteUserPhoto, getHomeUsers, getUserProfileById, updateProfile, updateUserProfile } from '../controllers/userController.js';
 
-
 const router = Router();
 
 router.get('/', verifyJwt, getHomeUsers);
-router.get('/:id', verifyJwt, getUserProfileById);
 
-router.put('/:id', verifyJwt, updateUserProfile);
+// ⚡️ Place tes routes spécifiques (/profile, /photos) AVANT les routes avec des paramètres dynamiques (/:id)
+router.put('/profile', verifyJwt, updateProfile);
 
 router.post('/photos', verifyJwt, addUserPhoto);
-
 router.delete('/photos/:photoId', verifyJwt, deleteUserPhoto);
-router.put('/profile', verifyJwt, updateProfile);
+
+// Les routes dynamiques en dessous
+router.get('/:id', verifyJwt, getUserProfileById);
+router.put('/:id', verifyJwt, updateUserProfile);
 
 export default router;
